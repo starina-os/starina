65e3b56946b79c3dedfc4bf0e4cf00fc75338129
Seiya Nuta <nuta@seiya.me>
2024-07-02 19:40:16 +0900

    Introduce virtio library

---
+++ libs/rust/virtio/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+pub mod transports;
+pub mod virtqueue;
+
+pub type DeviceType = u32;
+pub const VIRTIO_DEVICE_TYPE_NET: DeviceType = 1;
+pub const VIRTIO_DEVICE_TYPE_BLK: DeviceType = 2;
+
+#[derive(Debug)]
+pub enum VirtioAttachError {
+    UnexpectedDeviceType(DeviceType),
+    MissingFeatures,
+    MissingPciCommonCfg,
+    MissingPciDeviceCfg,
+    MissingPciIsrCfg,
+    MissingPciNotifyCfg,
+    FeatureNegotiationFailure,
+    NotSupportedBarType,
+}
