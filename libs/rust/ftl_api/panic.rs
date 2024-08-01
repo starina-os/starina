d0abda6091cf56b85a54ae15aab769e8c371c42d
Seiya Nuta <nuta@seiya.me>
2024-08-01 14:05:13 +0900

    [api] Make panic more human-readable

--- libs/rust/ftl_api/panic.rs
+++ libs/rust/ftl_api/panic.rs
@@ -4,6 +4,6 @@ use crate::println;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("panic: {}", info);
+    println!("[{}] \x1b[1;91mPANIC: {}\x1b[0m", ::core::module_path!(), info);
     loop {}
 }
