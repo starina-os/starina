44466f7f13e804789436d1063e152021f7443f5a
Seiya Nuta <nuta@seiya.me>
2024-09-27 21:12:04 +0900

    [http_server] Refactor

---
+++ apps/http_server/http.rs
@@ -0,0 +1,82 @@
+use ftl_api::channel::ChannelSender;
+use ftl_api::prelude::*;
+use httparse::Request;
+use httparse::Status;
+use httparse::EMPTY_HEADER;
+
+use crate::ftl_autogen::idl::tcpip::TcpSend;
+
+const REQUEST_MAX_SIZE: usize = 32 * 1024;
+
+fn do_handle_request(req: Request, tcp_sender: &ChannelSender) {
+    info!("{}, {}", req.method.unwrap(), req.path.unwrap());
+
+    let data = b"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, world!";
+    tcp_sender
+        .send(TcpSend {
+            data: data.as_slice().try_into().unwrap(),
+        })
+        .unwrap();
+}
+
+fn handle_request(buf: &[u8], tcp_sender: &ChannelSender) {
+    let mut headers = [EMPTY_HEADER; 32];
+    let mut req = Request::new(&mut headers);
+    match req.parse(buf) {
+        Ok(Status::Complete(_len)) => {
+            do_handle_request(req, tcp_sender);
+        }
+        Ok(Status::Partial) => {
+            warn!("unexpected httparse::Status::Partial");
+            return;
+        }
+        Err(e) => {
+            debug_warn!("error parsing request: {:?}", e);
+            return;
+        }
+    };
+}
+
+#[derive(Debug)]
+pub enum Conn {
+    ReceivingRequestHeaders { buf: Vec<u8> },
+    Finished,
+}
+
+impl Conn {
+    pub fn new() -> Self {
+        Conn::ReceivingRequestHeaders { buf: Vec::new() }
+    }
+
+    pub fn tcp_receive(&mut self, data: &[u8], tcp_sender: &ChannelSender) {
+        match self {
+            Conn::ReceivingRequestHeaders { buf } => {
+                let mut handled = false;
+                for line in data.split_inclusive(|&byte| byte == b'\n') {
+                    if buf.len() + line.len() > REQUEST_MAX_SIZE {
+                        warn!("request too large");
+                        *self = Conn::Finished;
+                        return;
+                    }
+
+                    buf.extend_from_slice(line);
+
+                    if buf.ends_with(b"\r\n\r\n") {
+                        handle_request(buf, tcp_sender);
+                        handled = true;
+                        break;
+                    }
+
+                    debug!("buf: {:?}", core::str::from_utf8(buf).unwrap());
+                }
+
+                if handled {
+                    *self = Conn::Finished;
+                }
+            }
+            Conn::Finished => {
+                // Discard the received data.
+            }
+        }
+    }
+}
--- apps/http_server/main.rs
+++ apps/http_server/main.rs
@@ -4,72 +4,23 @@
 ftl_api::autogen!();
 
 use ftl_api::channel::Channel;
-use ftl_api::channel::ChannelSender;
 use ftl_api::environ::Environ;
 use ftl_api::mainloop::Event;
 use ftl_api::mainloop::Mainloop;
 use ftl_api::prelude::*;
 use ftl_api::types::message::MessageBuffer;
 use ftl_autogen::idl::tcpip::TcpListen;
-use ftl_autogen::idl::tcpip::TcpSend;
 use ftl_autogen::idl::Message;
 
-#[derive(Debug)]
-struct Client {
-    buffered: Vec<u8>,
-}
-
-impl Client {
-    pub fn new() -> Client {
-        Client {
-            buffered: Vec::new(),
-        }
-    }
-
-    fn handle_request(&self, tcp_sender: &ChannelSender, req: httparse::Request<'_, '_>) {
-        trace!("request: {:?}", req);
-
-        let data = &b"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, world!"[..];
-        tcp_sender
-            .send(TcpSend {
-                data: data.try_into().unwrap(),
-            })
-            .unwrap();
-    }
-
-    pub fn receive(&mut self, tcp_sender: &ChannelSender, data: &[u8]) {
-        self.buffered.extend_from_slice(&data);
-
-        if let Some(index) = self.buffered.windows(4).position(|w| w == b"\r\n\r\n") {
-            let request_bytes = &self.buffered[..index + 4];
-
-            let mut headers = [httparse::EMPTY_HEADER; 32];
-            let mut req = httparse::Request::new(&mut headers);
-            match req.parse(request_bytes) {
-                Ok(httparse::Status::Complete(_len)) => {
-                    self.handle_request(tcp_sender, req);
-                    self.buffered = Vec::new();
-                }
-                Ok(httparse::Status::Partial) => {
-                    warn!("partial request");
-                    return;
-                }
-                Err(e) => {
-                    warn!("error parsing request: {:?}", e);
-                    return;
-                }
-            };
-        }
-    }
-}
+mod http;
 
 #[derive(Debug)]
 enum Context {
     Startup,
-    // TCP/IP control channel.
+    // TCP/IP listen channel (so-called TCP backlog).
     Listen,
     // TCP/IP data channel. Represents each TCP connection.
-    Conn(Client),
+    Conn(http::Conn),
 }
 
 #[no_mangle]
@@ -98,15 +49,15 @@ pub fn main(mut env: Environ) {
             } => {
                 let sock_ch = m.conn.take::<Channel>().unwrap();
                 mainloop
-                    .add_channel(sock_ch, Context::Conn(Client::new()))
+                    .add_channel(sock_ch, Context::Conn(http::Conn::new()))
                     .unwrap();
             }
             Event::Message {
-                ctx: Context::Conn(client),
+                ctx: Context::Conn(conn),
                 message: Message::TcpReceived(m),
                 sender,
             } => {
-                client.receive(sender, m.data.as_slice());
+                conn.tcp_receive(m.data.as_slice(), sender);
             }
             Event::Message {
                 ctx: Context::Conn(_),
