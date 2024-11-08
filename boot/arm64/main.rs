6ab9305f2ddcc0876f71cf5beea4831ffeb2602e
Seiya Nuta <nuta@seiya.me>
2024-11-08 20:51:08 +0900

    Use `&raw const`

--- boot/arm64/main.rs
+++ boot/arm64/main.rs
@@ -20,10 +20,10 @@ extern "C" {
 
 #[no_mangle]
 unsafe extern "C" fn arm64_boot() -> ! {
-    let bss_start = &__bss as *const _ as usize;
-    let bss_end = &__bss_end as *const _ as usize;
-    let free_ram = &__free_ram as *const _ as usize;
-    let free_ram_end = &__free_ram_end as *const _ as usize;
+    let bss_start = &raw const __bss as usize;
+    let bss_end = &raw const __bss_end as usize;
+    let free_ram = &raw const __free_ram as usize;
+    let free_ram_end = &raw const __free_ram_end as usize;
 
     // Clear bss section.
     core::ptr::write_bytes(bss_start as *mut u8, 0, bss_end - bss_start);
