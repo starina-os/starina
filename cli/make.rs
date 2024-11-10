55f4a0bcfacd43fa422f600b85f9b957ca36a3ee
Seiya Nuta <nuta@seiya.me>
2024-11-10 22:08:22 +0900

    Update

--- cli/make.rs
+++ cli/make.rs
@@ -1,10 +1,31 @@
+use std::env::current_dir;
+use std::env::set_current_dir;
 use std::fs;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
 
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
+
+fn move_to_project_dir() -> Result<()> {
+    // Look for the directory with Makefile.
+    let mut dir = current_dir()?;
+    loop {
+        if dir.join("Makefile").exists() && dir.join("Cargo.lock").exists() {
+            set_current_dir(&dir)
+                .with_context(|| format!("failed to chdir to {}", dir.display()))?;
+            return Ok(());
+        }
+
+        if !dir.pop() {
+            break;
+        }
+    }
+
+    bail!("failed to locate the project directory");
+}
 
 fn ensure_buildconfig() -> Result<()> {
     let buildconfig_path = Path::new("buildconfig.mk");
@@ -20,6 +41,7 @@ fn ensure_buildconfig() -> Result<()> {
 }
 
 pub fn run_make(cmd: &str) -> Result<()> {
+    move_to_project_dir();
     ensure_buildconfig()?;
     let err = Command::new("make").arg(cmd).exec();
     panic!("failed to exec make: {}", err);
