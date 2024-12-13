06ac0ab727fb92b0efeed6e5fb675557a3d5c0a2
Seiya Nuta <nuta@seiya.me>
2024-12-13 11:04:43 +0900

    Update

--- cli/minimake/depinfo_parser.rs
+++ cli/minimake/depinfo_parser.rs
@@ -2,34 +2,39 @@
 //!
 //! <https://doc.rust-lang.org/cargo/reference/build-cache.html#dep-info-files>
 
+use std::path::Path;
+
+#[derive(Debug)]
 pub enum Error {
     DelimiterNotFound,
 }
 
 pub struct DepInfoParser<'a> {
-    prerequisites: Vec<&'a str>,
+    prerequisites: Vec<&'a Path>,
 }
 
-impl DepInfoParser {
-    pub fn parse() -> Result<DepInfoParser, Error> {
+impl<'a> DepInfoParser<'a> {
+    pub fn parse(input: &'a str) -> Result<DepInfoParser<'a>, Error> {
         let mut prerequisites = Vec::new();
         let mut parts = input.split(':');
-        let target = parts.next().unwrap();
+        let _target = parts.next().unwrap();
         let prerequisites_str = parts.next().ok_or(Error::DelimiterNotFound)?;
-        for prerequisite in prerequisites_str.split_whitespace() {
-            prerequisites.push(prerequisite);
+        for p in prerequisites_str.split_whitespace() {
+            prerequisites.push(Path::new(p));
         }
 
         Ok(DepInfoParser { prerequisites })
     }
 
-    pub fn prerequisites(&self) -> &[&str] {
+    pub fn prerequisites(&self) -> &[&Path] {
         &self.prerequisites
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn real_world_dep_info() {
         let input = "/home/seiya/starina/target/arm64-user/release/tcpip: /home/seiya/starina/apps/servers/tcpip/main.rs /home/seiya/starina/libs/rust/starina/allocator.rs /home/seiya/starina/libs/rust/starina/arch/arm64/mod.rs /home/seiya/starina/libs/rust/starina/arch/arm64/start.S /home/seiya/starina/libs/rust/starina/arch/arm64/syscall.rs /home/seiya/starina/libs/rust/starina/arch/mod.rs /home/seiya/starina/libs/rust/starina/channel.rs /home/seiya/starina/libs/rust/starina/collections.rs /home/seiya/starina/libs/rust/starina/handle.rs /home/seiya/starina/libs/rust/starina/lib.rs /home/seiya/starina/libs/rust/starina/mainloop.rs /home/seiya/starina/libs/rust/starina/message.rs /home/seiya/starina/libs/rust/starina/panic.rs /home/seiya/starina/libs/rust/starina/poll.rs /home/seiya/starina/libs/rust/starina/print.rs /home/seiya/starina/libs/rust/starina/start.rs /home/seiya/starina/libs/rust/starina/syscall.rs /home/seiya/starina/libs/rust/types/error.rs /home/seiya/starina/libs/rust/types/handle.rs /home/seiya/starina/libs/rust/types/lib.rs /home/seiya/starina/libs/rust/types/message.rs /home/seiya/starina/libs/rust/types/poll.rs /home/seiya/starina/libs/rust/types/syscall.rs";
@@ -38,29 +43,29 @@ mod tests {
         assert_eq!(
             parser.prerequisites(),
             &[
-                "/home/seiya/starina/apps/servers/tcpip/main.rs",
-                "/home/seiya/starina/libs/rust/starina/allocator.rs",
-                "/home/seiya/starina/libs/rust/starina/arch/arm64/mod.rs",
-                "/home/seiya/starina/libs/rust/starina/arch/arm64/start.S",
-                "/home/seiya/starina/libs/rust/starina/arch/arm64/syscall.rs",
-                "/home/seiya/starina/libs/rust/starina/arch/mod.rs",
-                "/home/seiya/starina/libs/rust/starina/channel.rs",
-                "/home/seiya/starina/libs/rust/starina/collections.rs",
-                "/home/seiya/starina/libs/rust/starina/handle.rs",
-                "/home/seiya/starina/libs/rust/starina/lib.rs",
-                "/home/seiya/starina/libs/rust/starina/mainloop.rs",
-                "/home/seiya/starina/libs/rust/starina/message.rs",
-                "/home/seiya/starina/libs/rust/starina/panic.rs",
-                "/home/seiya/starina/libs/rust/starina/poll.rs",
-                "/home/seiya/starina/libs/rust/starina/print.rs",
-                "/home/seiya/starina/libs/rust/starina/start.rs",
-                "/home/seiya/starina/libs/rust/starina/syscall.rs",
-                "/home/seiya/starina/libs/rust/types/error.rs",
-                "/home/seiya/starina/libs/rust/types/handle.rs",
-                "/home/seiya/starina/libs/rust/types/lib.rs",
-                "/home/seiya/starina/libs/rust/types/message.rs",
-                "/home/seiya/starina/libs/rust/types/poll.rs",
-                "/home/seiya/starina/libs/rust/types/syscall.rs",
+                Path::new("/home/seiya/starina/apps/servers/tcpip/main.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/allocator.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/arch/arm64/mod.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/arch/arm64/start.S"),
+                Path::new("/home/seiya/starina/libs/rust/starina/arch/arm64/syscall.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/arch/mod.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/channel.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/collections.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/handle.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/lib.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/mainloop.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/message.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/panic.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/poll.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/print.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/start.rs"),
+                Path::new("/home/seiya/starina/libs/rust/starina/syscall.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/error.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/handle.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/lib.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/message.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/poll.rs"),
+                Path::new("/home/seiya/starina/libs/rust/types/syscall.rs"),
             ]
         );
 
---
+++ cli/minimake/mod.rs
@@ -0,0 +1,1 @@
+mod depinfo_parser;
