603bf4b8de6cb1ca9e8191f18afb6602e666909b
Seiya Nuta <nuta@seiya.me>
2025-02-22 10:29:27 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -eux
+set -eu
 
+QEMU=${QEMU:-qemu-system-riscv64}
+
+export CARGO_TERM_HYPERLINKS=false
 cargo build \
   -Z build-std=core,alloc \
   -Z build-std-features=compiler-builtins-mem \
   --target kernel/arch/riscv64/kernel.json \
   --manifest-path kernel/Cargo.toml
 
-qemu-system-riscv64 -machine virt -m 256 -bios default \
+$QEMU -machine virt -m 256 -bios default \
   -kernel target/kernel/debug/kernel \
   -nographic -serial mon:stdio --no-reboot \
   -d cpu_reset,unimp,guest_errors,int -D qemu.log
