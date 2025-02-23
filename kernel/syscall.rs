6df49d5be763483850f56a552a72c593e7d51bd7
Seiya Nuta <nuta@seiya.me>
2025-02-23 21:01:56 +0900

    Better typing

--- kernel/syscall.rs
+++ kernel/syscall.rs
@@ -20,15 +20,10 @@ pub extern "C" fn syscall_handler(
     a3: usize,
     a4: usize,
     a5: usize,
-) {
+) -> ! {
     trace!(
         "syscall_handler: a0={:x}, a1={:x}, a2={:x}, a3={:x}, a4={:x}, a5={:x}",
-        a0,
-        a1,
-        a2,
-        a3,
-        a4,
-        a5
+        a0, a1, a2, a3, a4, a5
     );
     switch_thread();
 }
