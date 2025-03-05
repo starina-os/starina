76d3c33be8f0e22e63a32464f1311f1dccee1c17
Seiya Nuta <nuta@seiya.me>
2025-03-05 12:33:17 +0900

    Update

--- libs/rust/starina/channel.rs
+++ libs/rust/starina/channel.rs
@@ -3,6 +3,7 @@ pub mod userspace {
     use alloc::boxed::Box;
     use alloc::vec::Vec;
     use core::mem::MaybeUninit;
+    use core::ptr::NonNull;
 
     use crate::error::ErrorCode;
     use crate::handle::HandleId;
@@ -10,6 +11,8 @@ pub mod userspace {
     use crate::handle::OwnedHandle;
     use crate::message::MESSAGE_DATA_LEN_MAX;
     use crate::message::MESSAGE_NUM_HANDLES_MAX;
+    use crate::message::MessageInfo;
+    use crate::syscall;
 
     pub struct Channel(OwnedHandle);
 
@@ -19,7 +22,19 @@ pub mod userspace {
         }
 
         pub fn recv(&self) -> Result<AnyMessage, ErrorCode> {
-            todo!()
+            let buffer = MessageBuffer::alloc();
+            let buffer_ptr: *mut MaybeUninit<MessageBuffer> = buffer.as_ptr();
+            let buffer_ptr = unsafe { (*buffer_ptr).as_mut_ptr() };
+            let data_ptr = unsafe { &mut (*buffer_ptr).data };
+            let handles_ptr = unsafe { &mut (*buffer_ptr).handles };
+            let msginfo = syscall::channel_recv(
+                self.0.id(),
+                data_ptr as *mut u8,
+                handles_ptr as *mut HandleId,
+            )?;
+
+            let msg = unsafe { AnyMessage::new(buffer, msginfo) };
+            Ok(msg)
         }
     }
 
@@ -39,30 +54,30 @@ pub mod userspace {
         spin::Mutex::new(Vec::new());
     const BUFFER_POOL_SIZE_MAX: usize = 16;
 
+    impl MessageBuffer {
+        pub fn alloc() -> NonNull<MaybeUninit<MessageBuffer>> {
+            if let Some(buffer) = GLOBAL_BUFFER_POOL.lock().pop() {
+                unsafe { NonNull::new_unchecked(Box::into_raw(buffer)) }
+            } else {
+                unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(MaybeUninit::uninit()))) }
+            }
+        }
+    }
+
     pub struct AnyMessage {
-        // msginfo: MessageInfo,
-        buffer: *mut MaybeUninit<MessageBuffer>,
+        msginfo: MessageInfo,
+        buffer: NonNull<MaybeUninit<MessageBuffer>>,
     }
 
     impl AnyMessage {
-        pub fn alloc() -> AnyMessage {
-            let buffer = if let Some(buffer) = GLOBAL_BUFFER_POOL.lock().pop() {
-                Box::into_raw(buffer)
-            } else {
-                // TODO: Skip zeroing out the buffer.
-                Box::into_raw(Box::new(MaybeUninit::uninit()))
-            };
-            AnyMessage { buffer }
-        }
-
-        pub unsafe fn raw_buffer(&self) -> *mut MessageBuffer {
-            unsafe { (*self.buffer).as_mut_ptr() }
+        unsafe fn new(buffer: NonNull<MaybeUninit<MessageBuffer>>, msginfo: MessageInfo) -> Self {
+            Self { buffer, msginfo }
         }
     }
 
     impl Drop for AnyMessage {
         fn drop(&mut self) {
-            let buffer = unsafe { Box::from_raw(self.buffer) };
+            let buffer = unsafe { Box::from_raw(self.buffer.as_ptr()) };
             let mut pool = GLOBAL_BUFFER_POOL.lock();
             if pool.len() < BUFFER_POOL_SIZE_MAX {
                 pool.push(buffer);
