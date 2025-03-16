e4808be4dbf4127749204deada9f39a75fce7631
Seiya Nuta <nuta@seiya.me>
2025-03-16 21:21:34 +0900

    Update

--- apps/tests/ktest/autogen.rs
+++
@@ -1,8 +0,0 @@
-use starina::syscall::VsyscallPage;
-
-use crate::App;
-
-// TODO: Remove this.
-pub fn app_main(vsyscall: *const VsyscallPage) {
-    starina::eventloop::app_loop::<(), App>(vsyscall);
-}
