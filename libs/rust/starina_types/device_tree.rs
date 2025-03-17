e260a41c9d3610a9165731df2c71db2268944f9a
Seiya Nuta <nuta@seiya.me>
2025-03-17 17:59:00 +0900

    Update

--- libs/rust/starina_types/device_tree.rs
+++ libs/rust/starina_types/device_tree.rs
@@ -1,9 +1,11 @@
 use alloc::borrow::ToOwned;
 use alloc::string::String;
+use alloc::vec;
 use alloc::vec::Vec;
 use core::slice;
 
 use fdt_rs::base::*;
+use fdt_rs::index::DevTreeIndex;
 use fdt_rs::prelude::*;
 use fdt_rs::spec::fdt_header;
 use hashbrown::HashMap;
@@ -13,14 +15,27 @@ use serde::Serialize;
 /// The device tree. This is the root of the device tree.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DeviceTree {
+    pub buses: HashMap<String, BusNode>,
     pub nodes: HashMap<String, DeviceNode>,
 }
 
 /// A node in the device tree.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DeviceNode {
-    pub compatible: Option<Vec<String>>,
-    pub reg: Option<Vec<(u64, u64)>>,
+    pub compatible: Vec<String>,
+    pub bus: String,
+    pub reg: Vec<Reg>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum BusNode {
+    NoMmu,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Reg {
+    pub addr: u64,
+    pub size: u64,
 }
 
 #[derive(Debug)]
@@ -31,6 +46,70 @@ pub enum ParseError {
     InvalidNode(fdt_rs::error::DevTreeError),
     InvalidName(fdt_rs::error::DevTreeError),
     InvalidProp(fdt_rs::error::DevTreeError),
+    InvalidLayout(fdt_rs::error::DevTreeError),
+    InvalidIndex(fdt_rs::error::DevTreeError),
+}
+
+fn stringlist_to_vec(
+    prop: &fdt_rs::index::DevTreeIndexProp<'_, '_, '_>,
+) -> Result<Vec<String>, ParseError> {
+    let mut values = Vec::new();
+    let mut iter = prop.iter_str();
+    while let Some(s) = iter.next().map_err(ParseError::InvalidProp)? {
+        values.push(s.to_owned());
+    }
+
+    Ok(values)
+}
+
+fn parse_reg(
+    regs: &mut Vec<Reg>,
+    prop: &fdt_rs::index::DevTreeIndexProp<'_, '_, '_>,
+    found_bus: &mut FoundBus,
+) -> Result<(), ParseError> {
+    let mut i = 0;
+    loop {
+        if prop.u32(i).is_err() {
+            // End of the regs.
+            break;
+        }
+
+        let addr = match found_bus.address_cells {
+            1 => prop.u32(i).map_err(ParseError::InvalidProp)? as u64,
+            2 => {
+                let high = prop.u32(i).map_err(ParseError::InvalidProp)? as u64;
+                let low = prop.u32(i + 1).map_err(ParseError::InvalidProp)? as u64;
+                (high << 32) | low
+            }
+            _ => {
+                panic!("unsupported address cells: {}", found_bus.address_cells);
+            }
+        };
+        i += found_bus.address_cells as usize;
+
+        let size = match found_bus.size_cells {
+            1 => prop.u32(i).map_err(ParseError::InvalidProp)? as u64,
+            2 => {
+                let high = prop.u32(i).map_err(ParseError::InvalidProp)? as u64;
+                let low = prop.u32(i + 1).map_err(ParseError::InvalidProp)? as u64;
+                (high << 32) | low
+            }
+            _ => {
+                panic!("unsupported size cells: {}", found_bus.size_cells);
+            }
+        };
+        i += found_bus.size_cells as usize;
+
+        regs.push(Reg { addr, size });
+    }
+
+    Ok(())
+}
+struct FoundBus {
+    is_referenced: bool,
+    bus: BusNode,
+    address_cells: u32,
+    size_cells: u32,
 }
 
 impl DeviceTree {
@@ -46,18 +125,107 @@ impl DeviceTree {
             DevTree::new(dtb).map_err(ParseError::InvalidTree)?
         };
 
-        let mut node_iter = devtree.nodes();
-        let mut root_nodes = HashMap::new();
-        while let Some(node) = node_iter.next().map_err(ParseError::InvalidNode)? {
-            let node_name = node.name().map_err(ParseError::InvalidName)?;
-            let mut prop_iter = node.props();
-            let mut compatible = None;
-            let mut reg = None;
-            while let Some(prop) = prop_iter.next().map_err(ParseError::InvalidProp)? {}
+        let layout = DevTreeIndex::get_layout(&devtree).map_err(ParseError::InvalidLayout)?;
+        let mut vec = vec![0u8; layout.size() + layout.align()];
+        let devtree_index =
+            DevTreeIndex::new(devtree, vec.as_mut_slice()).map_err(ParseError::InvalidIndex)?;
 
-            root_nodes.insert(node_name.to_owned(), DeviceNode { compatible, reg });
+        // Enumerate all buses.
+        let mut found_buses = HashMap::new();
+        for node in devtree_index.nodes() {
+            let node_name = node.name().map_err(ParseError::InvalidName)?;
+            let mut compatible = None;
+            let mut address_cells = None;
+            let mut size_cells = None;
+
+            for prop in node.props() {
+                let prop_name = prop.name().map_err(ParseError::InvalidName)?;
+                match prop_name {
+                    "compatible" => {
+                        compatible = Some(stringlist_to_vec(&prop)?);
+                    }
+                    "#address-cells" => {
+                        let value = prop.u32(0).map_err(ParseError::InvalidProp)?;
+                        address_cells = Some(value);
+                    }
+                    "#size-cells" => {
+                        let value = prop.u32(0).map_err(ParseError::InvalidProp)?;
+                        size_cells = Some(value);
+                    }
+                    _ => {}
+                }
+            }
+
+            let Some(compatible) = compatible else {
+                continue;
+            };
+            let Some(address_cells) = address_cells else {
+                continue;
+            };
+            let Some(size_cells) = size_cells else {
+                continue;
+            };
+
+            if compatible.iter().any(|c| c == "simple-bus") {
+                found_buses.insert(
+                    node_name.to_owned(),
+                    FoundBus {
+                        is_referenced: false,
+                        bus: BusNode::NoMmu,
+                        address_cells,
+                        size_cells,
+                    },
+                );
+            }
         }
 
-        Ok(DeviceTree { nodes: root_nodes })
+        let mut nodes = HashMap::new();
+        for node in devtree_index.nodes() {
+            let Some(parent_name) = node.parent().and_then(|p| p.name().ok()) else {
+                continue;
+            };
+
+            let Some(found_bus) = found_buses.get_mut(parent_name) else {
+                continue;
+            };
+
+            let node_name = node.name().map_err(ParseError::InvalidName)?;
+            let mut compatible = Vec::new();
+            let mut reg = Vec::new();
+            for prop in node.props() {
+                let prop_name = prop.name().map_err(ParseError::InvalidName)?;
+                match prop_name {
+                    "compatible" => {
+                        let mut iter = prop.iter_str();
+                        while let Some(s) = iter.next().map_err(ParseError::InvalidProp)? {
+                            compatible.push(s.to_owned());
+                        }
+                    }
+                    "reg" => {
+                        parse_reg(&mut reg, &prop, found_bus)?;
+                    }
+                    _ => {}
+                }
+            }
+
+            found_bus.is_referenced = true;
+            nodes.insert(
+                node_name.to_owned(),
+                DeviceNode {
+                    compatible,
+                    bus: parent_name.to_owned(),
+                    reg,
+                },
+            );
+        }
+
+        let mut buses = HashMap::new();
+        for (name, bus) in found_buses {
+            if bus.is_referenced {
+                buses.insert(name, bus.bus);
+            }
+        }
+
+        Ok(DeviceTree { nodes, buses })
     }
 }
