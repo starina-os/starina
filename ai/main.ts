226e751a69406e88a7f1ea1ed5ffaae0959b7f49
Seiya Nuta <nuta@seiya.me>
2025-04-10 10:59:58 +0900

    Update

---
+++ ai/main.ts
@@ -0,0 +1,12 @@
+import { generateText, generateObject } from 'ai';
+import { z } from 'zod';
+import { google } from "@ai-sdk/google"
+
+async function main() {
+
+}
+
+main().catch((error) => {
+    console.error("Error:", error);
+    process.exit(1);
+});
