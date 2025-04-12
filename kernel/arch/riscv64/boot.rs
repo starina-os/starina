6d486638d4b38151cd809e437da4658aec60741f
Seiya Nuta <nuta@seiya.me>
2025-04-12 21:29:22 +0900

    [kernel] Find free memory areas from device tree

--- kernel/arch/riscv64/boot.rs
+++ kernel/arch/riscv64/boot.rs
@@ -6,7 +6,6 @@ use arrayvec::ArrayVec;
 use super::get_cpuvar;
 use super::plic::use_plic;
 use crate::BootInfo;
-use crate::FreeRam;
 use crate::arch::riscv64::csr::StvecMode;
 use crate::arch::riscv64::csr::write_stvec;
 use crate::arch::riscv64::transition::switch_to_kernel;
@@ -37,34 +36,18 @@ unsafe extern "C" fn riscv64_boot(hartid: u64, dtb: *const u8) -> ! {
 unsafe extern "C" {
     static __bss: u8;
     static __bss_end: u8;
-    static __free_ram: u8;
-    static __free_ram_end: u8;
 }
 
 unsafe extern "C" fn rust_boot(hartid: u64, dtb: *const u8) -> ! {
-    let bss_start = &raw const __bss as usize;
-    let bss_end = &raw const __bss_end as usize;
-    let free_ram = &raw const __free_ram as usize;
-    let free_ram_end = &raw const __free_ram_end as usize;
-
     // Clear bss.
     unsafe {
+        let bss_start = &raw const __bss as usize;
+        let bss_end = &raw const __bss_end as usize;
         core::ptr::write_bytes(bss_start as *mut u8, 0, bss_end - bss_start);
     }
 
     let cpu_id = CpuId::new(hartid.try_into().unwrap());
-
-    let mut free_rams = ArrayVec::new();
-    free_rams.push(FreeRam {
-        addr: free_ram as *mut u8,
-        size: free_ram_end - free_ram,
-    });
-
-    crate::boot(BootInfo {
-        cpu_id,
-        free_rams,
-        dtb,
-    });
+    crate::boot(BootInfo { cpu_id, dtb });
 }
 
 pub fn percpu_init() {
