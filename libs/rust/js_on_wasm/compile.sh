bc6a5d7a570f8238123cb91f400a59f994cafe2d
Seiya Nuta <nuta@seiya.me>
2025-04-13 20:50:33 +0900

    Update

---
+++ libs/rust/js_on_wasm/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xue
+
+clang \
+    -O2 \
+    --target=wasm32-wasi \
+    --sysroot=/opt/homebrew/opt/wasi-libc/share/wasi-sysroot \
+    -nodefaultlibs \
+    -L libclang_rt.builtins-wasm32-wasi-24.0/ \
+    -DQJS_BUILD_LIBC \
+    -D_WASI_EMULATED_SIGNAL \
+    -D_GNU_SOURCE \
+    -lwasi-emulated-signal \
+    -lc \
+    -lclang_rt.builtins-wasm32 \
+    quickjs-amalgam.c main.c \
+    -o app.stage0.wasm
+
+wizer --allow-wasi -r _start=wizer.resume app.stage0.wasm -o app.stage1.wasm
+wasm-opt -O3 app.stage1.wasm -o app.optimized.wasm
--- libs/rust/js_on_wasm/main.c
+++ libs/rust/js_on_wasm/main.c
@@ -13,15 +13,17 @@ static JSContext *JS_NewCustomContext(JSRuntime *rt)
   return ctx;
 }
 
-// $ clang --target=wasm32-wasi --sysroot=/opt/homebrew/opt/wasi-libc/share/wasi-sysroot -L libclang_rt.builtins-wasm32-wasi-24.0/ -lc -DQJS_BUILD_LIBC quickjs-amalgam.c main.c -o app.wasm -O2 -D_WASI_EMULATED_SIGNAL -lwasi-emulated-signal -D_GNU_SOURCE -lclang_rt.builtins-wasm32 -nodefaultlibs
+// clang --target=wasm32-wasi --sysroot=/opt/homebrew/opt/wasi-libc/share/wasi-sysroot -L libclang_rt.builtins-wasm32-wasi-24.0/ -lc -DQJS_BUILD_LIBC quickjs-amalgam.c main.c -o app.stage0.wasm -O2 -D_WASI_EMULATED_SIGNAL -lwasi-emulated-signal -D_GNU_SOURCE -lclang_rt.builtins-wasm32 -nodefaultlibs && wizer --allow-wasi -r _start=wizer.resume app.stage0.wasm -o app.stage1.wasm && wasm-opt -O3 app.stage1.wasm -o app.optimized.wasm
 
-int main(void) {
-    JSRuntime *rt;
-    JSContext *ctx;
-    JSValue val;
+JSRuntime *rt;
+JSContext *ctx;
+JSValue val;
 
-    puts("starting quickjs...");
+bool initialized = false;
 
+__attribute__((export_name("wizer.initialize")))
+void wizer_initialize(void) {
+    puts("initializing quickjs...");
     rt = JS_NewRuntime();
     js_std_set_worker_new_context_func(JS_NewCustomContext);
     js_std_init_handlers(rt);
@@ -32,7 +34,11 @@ int main(void) {
         NULL
     };
     js_std_add_helpers(ctx, 0, argv);
+    initialized = true;
+}
 
+__attribute__((export_name("wizer.resume")))
+int wizer_resume(void) {
     puts("eval...");
     JS_Eval(ctx, APP_SCRIPT, strlen(APP_SCRIPT), "app.js", JS_EVAL_FLAG_STRICT);
 
