e1d65cf9868513f0bd4694df9bd2ed05199fa685
Seiya Nuta <nuta@seiya.me>
2025-04-27 15:14:41 +0900

    Update

--- kernel/src/arch/riscv64/cpuvar.rs
+++ kernel/src/arch/riscv64/cpuvar.rs
@@ -4,8 +4,11 @@ use super::thread::Context;
 use crate::refcount::SharedRef;
 use crate::thread::Thread;
 
+const CPUVAR_MAGIC: u64 = 0xc12f_c12f_c12f_c12f;
+
 pub struct CpuVar {
     pub context: *mut Context,
+    magic: u64,
     pub(super) kernel_sp: u64,
     pub(super) a0_scratch: u64,
 }
@@ -19,6 +22,7 @@ impl CpuVar {
         let sp_top = &raw const __boot_stack_top as u64;
         Self {
             context: unsafe { &raw mut (*idle_thread.arch_thread_ptr()).context },
+            magic: CPUVAR_MAGIC,
             kernel_sp: sp_top,
             a0_scratch: 0,
         }
@@ -31,10 +35,16 @@ pub fn get_cpuvar() -> &'static crate::cpuvar::CpuVar {
     unsafe {
         asm!("mv {}, tp", out(reg) cpuvar);
     }
+
+    debug_assert!(unsafe { cpuvar.is_null() } == false);
+    debug_assert!(unsafe { (*cpuvar).arch.magic } == CPUVAR_MAGIC);
     unsafe { &*cpuvar }
 }
 
 pub fn set_cpuvar(cpuvar: *const crate::cpuvar::CpuVar) {
+    debug_assert!(unsafe { cpuvar.is_null() } == false);
+    debug_assert!(unsafe { (*cpuvar).arch.magic } == CPUVAR_MAGIC);
+
     // Store the address of the current CPU's `CpuVar` to `tp`.
     unsafe {
         asm!("mv tp, {}", in(reg) cpuvar);
