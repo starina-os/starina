f92071f0dddc18fb0ab3ccaf824ec2bf5ede0255
Seiya Nuta <nuta@seiya.me>
2025-05-06 12:58:53 +0900

    Update

--- apps/servers/lx/lxinit/src/main.rs
+++ apps/servers/lx/lxinit/src/main.rs
@@ -1,3 +1,6 @@
+use nix::mount::MsFlags;
+use nix::mount::mount;
+
 fn main() {
     println!("");
     println!("");
@@ -6,4 +9,20 @@ fn main() {
     println!("");
     println!("");
     println!("");
+
+    mount(
+        Some("virtfs"),
+        "/virtfs",
+        Some("virtiofs"),
+        MsFlags::empty(),
+        None as Option<&str>,
+    )
+    .expect("failed to mount virtio-fs");
+
+    // List files in /virtfs
+    println!("Listing files in /virtfs:");
+    let files = std::fs::read_dir("/virtfs").expect("failed to read /virtfs");
+    for file in files {
+        println!("{}", file.unwrap().path().display());
+    }
 }
