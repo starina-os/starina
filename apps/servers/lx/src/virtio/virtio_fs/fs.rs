e39ac92f252486d55f3a593e1634eef2d1a0217b
Seiya Nuta <nuta@seiya.me>
2025-05-13 09:56:45 +0900

    Update

--- apps/servers/lx/src/virtio/virtio_fs/fs.rs
+++ apps/servers/lx/src/virtio/virtio_fs/fs.rs
@@ -32,7 +32,6 @@ pub struct INode(pub u64);
 
 impl INode {
     pub const fn new(id: u64) -> Self {
-        debug_assert!(id > 1, "non-root inode must be greater than 1");
         Self(id)
     }
 
