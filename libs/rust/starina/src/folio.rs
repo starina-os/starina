b38bdb8e51a157b6f24365d96367516bef68ef1d
Seiya Nuta <nuta@seiya.me>
2025-05-14 10:10:32 +0900

    [starina] Impl `Handleable` for `Folio`

--- libs/rust/starina/src/folio.rs
+++ libs/rust/starina/src/folio.rs
@@ -6,9 +6,11 @@ use starina_types::handle::HandleId;
 use starina_types::vmspace::PageProtect;
 use starina_utils::alignment::is_aligned;
 
+use crate::handle::Handleable;
 use crate::handle::OwnedHandle;
 use crate::iobus::IoBus;
 use crate::syscall;
+use crate::vmspace::SELF_VMSPACE;
 
 /// The ownership of a contiguous page-aliged memory region.
 ///
@@ -53,7 +55,11 @@ impl Folio {
     }
 }
 
-const SELF_VMSPACE: HandleId = HandleId::from_raw(0);
+impl Handleable for Folio {
+    fn handle_id(&self) -> HandleId {
+        self.handle.id()
+    }
+}
 
 pub struct MmioFolio {
     _folio: Folio,
