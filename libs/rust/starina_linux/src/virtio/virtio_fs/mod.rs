7895a896d0dbc10fe10d1a1ba5c9e4b355958264
Seiya Nuta <nuta@seiya.me>
2025-05-24 20:21:39 +0900

    Improve error handling

--- libs/rust/starina_linux/src/virtio/virtio_fs/mod.rs
+++ libs/rust/starina_linux/src/virtio/virtio_fs/mod.rs
@@ -8,3 +8,12 @@ pub use fs::INodeNo;
 pub use fs::ReadCompleter;
 pub use fs::ReadDirCompleter;
 pub use fs::ReadResult;
+use thiserror::Error;
+
+use crate::guest_memory;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("failed to access guest memory: {0}")]
+    GuestMemory(#[from] guest_memory::Error),
+}
