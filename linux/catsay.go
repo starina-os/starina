3284b94c3955d7e3a4aad57de92a7389892b8cbd
Seiya Nuta <nuta@seiya.me>
2025-06-09 22:46:32 +0900

    wip

--- linux/catsay.go
+++ linux/catsay.go
@@ -4,11 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
+	fmt.Fprintln(os.Stderr, "catsay is starting")
+
 	reader := bufio.NewReader(os.Stdin)
 	messageBytes, _ := io.ReadAll(reader)
 	message := strings.TrimSpace(string(messageBytes))
@@ -22,4 +26,28 @@ func main() {
 	fmt.Println("  /\\_/\\  /")
 	fmt.Println(" ( o.o )")
 	fmt.Println(" \\(___)")
+
+	// Print to stderr
+	fmt.Fprintln(os.Stderr, "catsay is working")
+	fmt.Fprintln(os.Stderr, `
+  /\_/\
+ ( o.o )
+  > ^ <  `)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		catArt := `
+      ____________
+     < Hello Web! >
+      ------------
+          /
+  /\_/\  /
+ ( o.o )
+ \(___)
+`
+		fmt.Fprint(w, catArt)
+	})
+
+	fmt.Fprintln(os.Stderr, "Starting cat server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
