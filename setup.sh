cf7db0c03e2abd54a01ccbd8c5e4ec87575b6d21
Seiya Nuta <nuta@seiya.me>
2025-06-24 22:50:36 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -15,6 +15,16 @@ setup_linux() {
         golang-go lld
 }
 
+common_setup() {
+    if ! command -v rustup &> /dev/null; then
+        echo "rustup is not installed - visit https://rustup.rs" >&2
+        exit 1
+    fi
+
+    set -x
+    rustup target add riscv64gc-unknown-linux-musl
+}
+
 case "$(uname)" in
     Darwin)
         setup_macos
