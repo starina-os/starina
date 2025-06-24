b39fabddc63346c1df17dfe692be540c0228dceb
Seiya Nuta <nuta@seiya.me>
2025-06-24 22:19:46 +0900

    Update

--- linux/build.rs
+++ linux/build.rs
@@ -10,44 +10,26 @@ fn main() {
         return;
     }
 
-    let docker_info = Command::new("docker").arg("info").output();
-    if docker_info.is_err() || !docker_info.unwrap().status.success() {
-        panic!("Docker is not available. Please start Docker to continue.");
-    }
-
-    println!("cargo:rerun-if-changed=Dockerfile");
-    println!("cargo:rerun-if-changed=build-linux.sh");
+    println!("cargo:rerun-if-changed=Makefile");
     println!("cargo:rerun-if-changed=linux.riscv64.config");
     println!("cargo:rerun-if-changed=linuxinit");
     println!("cargo:rerun-if-changed=catsay.go");
 
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("failed to get manifest directory");
-    let build_status = Command::new("docker")
-        .arg("build")
-        .arg("--progress=plain")
-        .arg("-t")
-        .arg("starina-linux")
-        .arg(".")
+
+    let program = if cfg!(target_os = "macos") {
+        "make"
+    } else {
+        "docker"
+    };
+
+    let status = Command::new(program)
+        .arg("linux.elf")
         .current_dir(&manifest_dir)
-        .env("DOCKER_BUILDKIT", "1")
         .status()
         .expect("failed to execute docker build");
 
-    if !build_status.success() {
+    if !status.success() {
         panic!("Docker build failed");
-    }
-
-    let run_status = Command::new("docker")
-        .arg("run")
-        .arg("--rm")
-        .arg("-v")
-        .arg(format!("{manifest_dir}:/linux"))
-        .arg("starina-linux")
-        .current_dir(&manifest_dir)
-        .status()
-        .expect("failed to execute docker run");
-
-    if !run_status.success() {
-        panic!("Docker run failed");
     }
 }
