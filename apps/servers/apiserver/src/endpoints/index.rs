51db9495bcd92574ee290037089d899512470a35
Seiya Nuta <nuta@seiya.me>
2025-07-18 08:42:16 +0900

    [apiserver] Return Hello World page on /

--- apps/servers/apiserver/src/endpoints/index.rs
+++ apps/servers/apiserver/src/endpoints/index.rs
@@ -1,18 +1,18 @@
-use starina::prelude::*;
-
+use crate::http::HeaderName;
 use crate::http::Request;
 use crate::http::ResponseWriter;
 use crate::http::StatusCode;
 
-pub fn handle_index(req: &Request, resp: &mut impl ResponseWriter) -> anyhow::Result<()> {
+static INDEX_HTML: &[u8] = include_bytes!("../../shell/index.html");
+
+pub fn handle_index(_req: &Request, resp: &mut impl ResponseWriter) -> anyhow::Result<()> {
+    let headers = resp.headers_mut();
+    headers
+        .insert(HeaderName::CONTENT_TYPE, "text/html")
+        .unwrap();
+
     resp.write_headers(StatusCode::OK);
-    resp.write_body(
-        format!(
-            "Hello from Starina API! You requested: {} {}",
-            req.method, req.path
-        )
-        .as_bytes(),
-    );
+    resp.write_body(INDEX_HTML);
 
     Ok(())
 }
