70981bca9905f213020cfdd3a7e08c4a23cb328a
Seiya Nuta <nuta@seiya.me>
2025-07-26 14:06:26 +0900

    Drop `exit-on-idle` feature

--- kernel/src/arch/riscv64/idle.rs
+++ kernel/src/arch/riscv64/idle.rs
@@ -25,40 +25,6 @@ fn resume_from_idle() -> ! {
 }
 
 pub fn halt() -> ! {
-    if cfg!(feature = "exit-on-idle") {
-        // Use semihosting to shutdown the system.
-        trace!("exiting with semihosting call...");
-
-        #[repr(C, packed)]
-        struct ExitParams {
-            reason: u64,
-            exit_code: u64,
-        }
-
-        let params = ExitParams {
-            reason: 0x20026, // ADP_Stopped_ApplicationExit
-            exit_code: 122,
-        };
-
-        unsafe {
-            asm!(
-                ".balign 16",
-                ".option push",
-                ".option norvc", // Do not use compact instructions.
-
-                // Special sequence to trigger a semihosting syscall.
-                "slli x0, x0, 0x1f",
-                "ebreak",
-                "srai x0, x0, 7",
-
-                ".option pop",
-
-                in("a0") 0x18, // SYS_EXIT
-                in("a1") &raw const params as usize,
-            );
-        }
-    }
-
     loop {
         unsafe {
             asm!("wfi");
@@ -67,10 +33,6 @@ pub fn halt() -> ! {
 }
 
 pub fn idle() -> ! {
-    if cfg!(feature = "exit-on-idle") {
-        halt();
-    }
-
     unsafe {
         write_stvec(idle_entry as *const () as usize, StvecMode::Direct);
 
